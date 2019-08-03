6ef65100a2099fa438ea0ece4037f0bd3bccff03
wpp <827694763@qq.com>
2019-08-03 16:38:12 +0800

    oneself

---
+++ HolidayHomeWork03.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//打印空心正方形
+public class HolidayHomeWork03{
+    public static void main(String[] args){
+        for(int i=0;i<11;i++){
+			if(i == 0 || i ==10){
+				System.out.println("* * * * * * * * * * *");
+			}else{
+				System.out.println("*                   *");
+			}
+		}
+
+	}
+}
