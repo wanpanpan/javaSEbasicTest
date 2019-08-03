d20fe42fde1d4ed0e799be246c0a4dd125aa75f5
wpp <827694763@qq.com>
2019-08-03 14:13:41 +0800

    oneself

---
+++ Pratice3.java
@@ -0,0 +1,8 @@
+public class Pratice3{
+    public static void main(String[] args){
+	    int num = 19;
+		System.out.println(Integer.toBinaryString(num));
+		System.out.println(Integer.toHexString(num));
+		System.out.println(Integer.toOctalString(num));
+	}
+}
