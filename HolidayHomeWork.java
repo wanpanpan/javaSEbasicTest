92046f0761b8feb0a880fa505f0ffdc3232d9134
wpp <827694763@qq.com>
2019-08-05 19:54:28 +0800

    daima

---
+++ HolidayHomeWork.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class HolidayHomeWork04{
+    public static void main(String[] args){
+	    for(int i=1;i<=9;i++){
+		    for(int j=1;j<=i;j++){
+			    System.out.print( i + "*" + j + "=" + (j*i) + " ");
+		    }
+			System.out.println();
+		}
+	}
+}
