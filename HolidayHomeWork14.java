e4b24d0a147cf4947505b161b6013b1165403988
wpp <827694763@qq.com>
2019-08-05 20:14:43 +0800

    qpppp

---
+++ HolidayHomeWork14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//有n个人围成一圈,顺序排号,
+//一个人开始报数(从1到3报数)
+//道3的人退出圈子,
+//问最后留下的是原来第几号的那位
+public class HolidayHomeWork14{
+    public static void main(String[] args){
+	    int i = 15;
+	    for(int a = 1;a<=i;a++){
+		    if((a%3) != 0){
+                            System.out.print(a);
+		    }
+
+		}
+	}
+}
