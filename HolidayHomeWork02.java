d79e74ded886f90bb697e803c14d06787c9fafb1
wpp <827694763@qq.com>
2019-08-05 19:57:11 +0800

    daima

---
+++ HolidayHomeWork02.java
@@ -0,0 +1,10 @@
+public class HolidayHomeWork02{
+// ¼ÆËã10µÄ½×³Ë
+    public static void main(String[] args){
+	    int sum = 1;
+		for(int i = 1;i<=10;i++){
+		    sum = sum*i;
+		}
+		System.out.print(sum);
+	}
+}
